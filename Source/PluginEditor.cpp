ef1908062d5924d9fa5f8ca15fd206d2eda281bd
Oles <oles1stepanov@gmail.com>
2023-05-11 08:37:21 +0300

    Implement Sceleton for sliders

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -128,7 +128,17 @@ void ResponseCurveComponent ::paint (juce::Graphics& g)
 
 //==============================================================================
 TestEqAudioProcessorEditor::TestEqAudioProcessorEditor (TestEqAudioProcessor& p)
-    : AudioProcessorEditor (&p), audioProcessor (p), responseCurveComponent(audioProcessor),
+    : AudioProcessorEditor (&p), audioProcessor (p),
+
+            peakFreqSlider(*audioProcessor.apvts.getParameter("Peak Freq"), "Hz"),
+            peakGainSlider(*audioProcessor.apvts.getParameter("Peak Gain"), "dB"),
+            peakQualitySlider(*audioProcessor.apvts.getParameter("Peak Q"), ""),
+            lowCutFreqSlider(*audioProcessor.apvts.getParameter("LowCut Freq"), "Hz"),
+            highCutFreqSlider(*audioProcessor.apvts.getParameter("HighCut Freq"), "Hz"),
+            lowCutSlopeSlider(*audioProcessor.apvts.getParameter("LowCut Slope"), "dB/Oct"),
+            highCutSlopeSlider(*audioProcessor.apvts.getParameter("High Slope"), "dB/Oct"),
+
+            responseCurveComponent(audioProcessor),
             peakFreqSliderAttachment(audioProcessor.apvts, "Peak Freq", peakFreqSlider),
             peakGainSliderAttachment(audioProcessor.apvts, "Peak Gain", peakGainSlider),
             peakQualitySliderAttachment(audioProcessor.apvts, "Peak Q", peakQualitySlider),
--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -11,13 +11,38 @@
 #include <JuceHeader.h>
 #include "PluginProcessor.h"
 
-struct CustomRotarySlider : juce::Slider
+struct LookAndFeel : juce::LookAndFeel_V4
 {
-    CustomRotarySlider(): juce::Slider(juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag,
-                                       juce::Slider::TextEntryBoxPosition::NoTextBox)
+    void drawRotarySlider(juce::Graphics&,
+                          int x, int y, int with, int height,
+                          float sliderPosProportional,
+                          float rotaryStartAngle,
+                          float rotaryEndEngle,
+                          juce::Slider&) override { }
+    
+};
+
+struct RotarySliderWithLabels : juce::Slider
+{
+    RotarySliderWithLabels(juce::RangedAudioParameter& rap, const juce::String unitSuffix) : juce::Slider(juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag, juce::Slider::TextEntryBoxPosition::NoTextBox),
+        param(&rap), suffix(unitSuffix)
     {
-        
+        setLookAndFeel(&lnf);
     }
+    ~RotarySliderWithLabels()
+    {
+        setLookAndFeel(nullptr);
+    }
+    
+    void paint (juce::Graphics& g) override {};
+    juce::Rectangle<int>getSliderBounds() const;
+    int getTextHeight() const {return 14;}
+    juce::String getDisplayString() const;
+    
+private:
+    LookAndFeel lnf;
+    juce::RangedAudioParameter* param;
+    juce::String suffix;
 };
 
 struct ResponseCurveComponent : juce::Component, juce::AudioProcessorParameter::Listener, juce::Timer
@@ -57,7 +82,7 @@ private:
     // access the processor object that created it.
     TestEqAudioProcessor& audioProcessor;
     
-    CustomRotarySlider peakFreqSlider,
+    RotarySliderWithLabels peakFreqSlider,
                         peakGainSlider,
                         peakQualitySlider,
                         lowCutFreqSlider,
