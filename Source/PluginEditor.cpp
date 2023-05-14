fa9a9d5a97e9de7420df56a12344ff5278842a40
Oles <oles1stepanov@gmail.com>
2023-05-14 14:40:21 +0300

    Addad freq labels

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -300,9 +300,9 @@ void ResponseCurveComponent::resized()
     
     Array<float> freq
     {
-        20, 30, 40, 50, 100,
-        200, 300, 400, 500, 1000,
-        2000, 3000, 4000, 5000, 10000,
+        20, /*30, 40,*/ 50, 100,
+        200, /*300, 400,*/ 500, 1000,
+        2000, /*3000, 4000,*/ 5000, 10000,
         20000
     };
     
@@ -344,6 +344,38 @@ void ResponseCurveComponent::resized()
     }
     
 //    g.drawRect(getAnalysisAre());
+    g.setColour(Colours::lightgrey);
+    const int fontHeight = 10;
+    g.setFont(fontHeight);
+    
+    for (int i = 0; i < freq.size(); ++i)
+    {
+        auto f = freq[i];
+        auto x = xs[i];
+        
+        bool addK = false;
+        String str;
+        if (f > 999.f)
+        {
+            addK = true;
+            f /= 1000.f;
+        }
+        
+        str << f;
+        if (addK)
+            str << "k";
+        str << "Hz";
+        
+        auto textWidth = g.getCurrentFont().getStringWidth(str);
+        
+        Rectangle<int> r;
+        r.setSize(textWidth, fontHeight);
+        r.setCentre(x, 0);
+        r.setY(1);
+        
+        g.drawFittedText(str, r, juce::Justification::centred, 1);
+        
+    }
 }
 
 juce::Rectangle<int> ResponseCurveComponent::getRenderArea()
